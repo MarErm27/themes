989d4a35e062db5f15affe21da84509f9ba8434e
MarErm27 <v27i72@gmail.com>
2020-09-17 09:24:45 +0300

    update for a custom JS script with template.go

--- adminlte/template.go
+++ adminlte/template.go
@@ -2844,7 +2844,7 @@ function emailFunction() {
         //alert(myArray.length - 1);
     });
 }
-$("tbody").ready(emailFunction)
+$("section.content").ready(emailFunction)
 	</script>
     </body>
     {{if not .Iframe}}
